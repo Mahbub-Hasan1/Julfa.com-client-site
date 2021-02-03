654535063355c69d68fa5e9043c114692e164549
Md-Mahbub-Hasan <trylife.mahbub@gmail.com>
2021-02-02 17:20:40 -0800

    This is my shoping site

---
+++ src/Component/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>আল্লাহু আকবার । বিসমিল্লাহ হির রাহমানির রাহিম। আল্লাহু আকবার।</h1>
+        </div>
+    );
+};
+
+export default Home;
